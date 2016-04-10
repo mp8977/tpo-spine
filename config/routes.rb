8fb8cd5c29df442e20847c249e6a0ba57fe6cbc0
mp8977 <mp8977@student.uni-lj.si>
2016-04-10 15:00:55 +0200

    Kreiranje vstopne staticne strani
    - homepage nase aplikacije, ki nam da opcijo da izberemo tip uporabnika,
    s katerim se zelimo vpisati na stran

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'static/home'
+
   # --> Must be ABOVE 'resources' !!!
   devise_for :users,
              controllers: { sessions: "users/sessions", registrations: "users/registrations",
@@ -53,7 +55,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

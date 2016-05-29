ea16a5a0597da2dd8638dcb7bb08d79ecae73471
mb5975 <mb5975@student.uni-lj.si>
2016-05-29 18:13:23 +0200

    dodan gumb za izbris kategorije

--- app/assets/javascripts/check_ups.coffee
+++ app/assets/javascripts/check_ups.coffee
@@ -34,7 +34,3 @@ jQuery ->
     regexp=new RegExp($(this).data('id'), 'g')
     $(this).before($(this).data('fields').replace(regexp,time))
     event.preventDefault()
-
-  $('form').on 'click', '.remove_fields', (event) ->
-    $(this).closest('fieldset').remove()
-    event.preventDefault()
--- app/helpers/check_ups_helper.rb
+++ app/helpers/check_ups_helper.rb
@@ -1,31 +1,36 @@
 module CheckUpsHelper
   def link_to_add_fields(name, f, association)
     if name == "Dodaj meritve za srce"
-      new_object = f.object.send(association).klass.new
-      id = new_object.object_id
-      fields = f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
-      end
-      link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+      fields = "<div>"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
       end
-      link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+      #link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
       end
-      link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+      #link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+      new_object = f.object.send(association).klass.new
+      id = new_object.object_id
+      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
+        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
+      end
+      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
+      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
+
     elsif name == "Dodaj meritve za glukozo"
+      fields = "<div>"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
-      fields = f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
+      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
       end
-      link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
+      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
     end
   end
 end

a7512c320f9894569f2c4ea37187505122ed2b56
mb5975 <mb5975@student.uni-lj.si>
2016-05-29 19:41:14 +0200

    dodani hidden fields za ime in enoto

--- app/helpers/check_ups_helper.rb
+++ app/helpers/check_ups_helper.rb
@@ -2,23 +2,22 @@ module CheckUpsHelper
   def link_to_add_fields(name, f, association)
     if name == "Dodaj meritev za srce"
       fields = "<div>"
-      fields +="Srce<br>"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
+        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Srcni utrip', u: 'stevilo srcnih utripov v minuti')
       end
       #link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
+        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Sistolicni tlak', u: 'mmHg')
       end
       #link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
+        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Diastolicni tlak', u: 'mmHg')
       end
       fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
@@ -27,7 +26,7 @@ module CheckUpsHelper
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
+        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Glukoza', u: 'mmol/L')
       end
       fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
@@ -36,7 +35,7 @@ module CheckUpsHelper
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
+        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Temperatura', u: 'stopinj celzija')
       end
       fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
@@ -45,7 +44,7 @@ module CheckUpsHelper
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form)
+        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Teza', u: 'kg')
       end
       fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})

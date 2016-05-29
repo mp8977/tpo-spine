108a56e347011ce6ff914ae0360efd06452c8a36
mb5975 <mb5975@student.uni-lj.si>
2016-05-29 12:50:52 +0200

    dodan link za brisanje elementa meritve

--- app/models/measurement_doc.rb
+++ app/models/measurement_doc.rb
@@ -3,6 +3,6 @@ class MeasurementDoc < ActiveRecord::Base
 	belongs_to :check_up
 
 
-	accepts_nested_attributes_for :part_measurement, :allow_destroy => true, reject_if: proc { |a| a['name'].blank? }
+	accepts_nested_attributes_for :part_measurement, :allow_destroy => true, reject_if: proc { |a| a['value'].blank? }
 	
 end

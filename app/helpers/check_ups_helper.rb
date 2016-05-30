d58a119462f908fdd24e8ded6f8f30deb2cf0cf1
nv94 <lavica.flow@gmail.com>
2016-05-30 08:42:27 +0200

    dodajanje kategorij

--- app/helpers/check_ups_helper.rb
+++ app/helpers/check_ups_helper.rb
@@ -1,7 +1,7 @@
 module CheckUpsHelper
   def link_to_add_fields(name, f, association)
     if name == "Dodaj meritev za srce"
-      fields = "<div>"
+      fields = "<div  data-no-turbolink >"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
@@ -19,35 +19,35 @@ module CheckUpsHelper
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Diastolicni tlak', u: 'mmHg')
       end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
-      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
+      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true, id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za glukozo"
-      fields = "<div>"
+      fields = "<div  data-no-turbolink >"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Glukoza', u: 'mmol/L')
       end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
-      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
+      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za telesno temperaturo"
-      fields = "<div>"
+      fields = "<div  data-no-turbolink >"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Temperatura', u: 'stopinj celzija')
       end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
-      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
+      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za telesno težo"
-      fields = "<div>"
+      fields = "<div  data-no-turbolink >"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Teza', u: 'kg')
       end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
-      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
+      link_to(name, '', class: "add_fields ", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     end
   end
 end

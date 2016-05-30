8e42331daaa508e8a438697b266e7fdfd9d8806a
nv94 <lavica.flow@gmail.com>
2016-05-30 23:05:09 +0200

    dodajanje presledkov in data-no-turbolinks

--- app/helpers/check_ups_helper.rb
+++ app/helpers/check_ups_helper.rb
@@ -19,7 +19,7 @@ module CheckUpsHelper
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Diastolicni tlak', u: 'mmHg')
       end
-      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true, id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za glukozo"
       fields = "<div  data-no-turbolink >"
@@ -28,7 +28,7 @@ module CheckUpsHelper
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Glukoza', u: 'mmol/L')
       end
-      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za telesno temperaturo"
       fields = "<div  data-no-turbolink >"
@@ -37,7 +37,7 @@ module CheckUpsHelper
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Temperatura', u: 'stopinj celzija')
       end
-      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za telesno težo"
       fields = "<div  data-no-turbolink >"
@@ -46,8 +46,8 @@ module CheckUpsHelper
       fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
         render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Teza', u: 'kg')
       end
-      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div><br>"
-      link_to(name, '', class: "add_fields ", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
+      fields += "<br><button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
+      link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     end
   end
 end

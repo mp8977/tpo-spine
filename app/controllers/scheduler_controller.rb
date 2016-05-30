56b45d1c8e661e604a50a33d282a6f4424aa04b8
dl4184 <dl4184@student.uni-lj.si>
2016-05-30 23:12:44 +0200

    Urnik 2. del

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -3,6 +3,32 @@ class SchedulerController < ApplicationController
 
   end
   def create
-    
+    schedulerHours=params[:scheduler][:hours]
+    schedulerStartDate=params[:scheduler][:startDay]
+    schedulerWeeks=params[:scheduler][:weeks]
+    duration=params[:scheduler][:duration]
+
+    puts(schedulerWeeks)
+    puts(duration)
+
+    schedulerHours1D=schedulerHours[2..-3].split("],[")
+    schedulerHours2D=[]
+    schedulerHours1D.each do |el|
+      schedulerHours2D.push(el.split(","))
+    end
+
+    i=0
+    while i<7
+      if  schedulerHours2D[i]!=nil
+        schedulerHours2D[i].each do |el|
+          date=DateTime.strptime(schedulerStartDate, '%d.%m.%Y')+i
+          date=date.change(hour:el[0..1].to_i,min:el[3..4].to_i)
+          puts(current_doctor.id)
+
+        end
+      end
+      i+=1
+    end
+
   end
 end

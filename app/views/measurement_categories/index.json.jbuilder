aae1235f3c164bd5063022c5c38fb3de9136cac2
mb5975 <mb5975@student.uni-lj.si>
2016-05-31 16:10:38 +0200

    dodane kategorije meritev v sifrante

--- app/views/measurement_categories/index.json.jbuilder
+++ app/views/measurement_categories/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@measurement_categories) do |measurement_category|
-  json.extract! measurement_category, :id, :name, :elements
+  json.extract! measurement_category, :id, :name, :elements, :deleted
   json.url measurement_category_url(measurement_category, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531072552) do
+ActiveRecord::Schema.define(version: 20160531134841) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "streetName",   limit: 255
@@ -201,8 +201,9 @@ ActiveRecord::Schema.define(version: 20160531072552) do
   create_table "measurement_categories", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.integer  "elements",   limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "deleted",                default: false, null: false
   end
 
   create_table "measurement_docs", force: :cascade do |t|

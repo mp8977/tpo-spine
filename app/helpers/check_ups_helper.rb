617328afbc4fbdfaa34a33172aac9796db4ad3ba
mb5975 <mb5975@student.uni-lj.si>
2016-05-31 11:05:02 +0200

    moja verzija helperja, ki ima se funkcionalnosti zgodbe #5

--- app/helpers/check_ups_helper.rb
+++ app/helpers/check_ups_helper.rb
@@ -1,52 +1,125 @@
 module CheckUpsHelper
   def link_to_add_fields(name, f, association)
     if name == "Dodaj meritev za srce"
+      st_elementov = MeasurementCategory.where(name: :srce).first.elements
+      category_id = MeasurementCategory.where(name: :srce).first.id
+      imena = []
+      enote = []
+      maxs = []
+      mins = []
+      MeasurementType.where(measurement_category_id: category_id).all.each do |m|
+        imena.push(m.name)
+        enote.push(m.unit)
+        maxs.push(m.max_value)
+        mins.push(m.min_value)
+      end
+
+
       fields = "<div data-no-turbolink>"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
-      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Srcni utrip', u: 'stevilo srcnih utripov v minuti')
+
+      for i in 0..(st_elementov-1)
+        fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
+          ime = imena[i]
+          enota = enote[i]
+          min_value = mins[i]
+          max_value = maxs[i]
+          render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: ime, u: enota, min: min_value, max: max_value)
+        end
       end
-      #link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
-      new_object = f.object.send(association).klass.new
-      id = new_object.object_id
-      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Sistolicni tlak', u: 'mmHg')
-      end
-      #link_to(name, '', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
-      new_object = f.object.send(association).klass.new
-      id = new_object.object_id
-      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Diastolicni tlak', u: 'mmHg')
-      end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
+
+      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za glukozo"
+      st_elementov = MeasurementCategory.where(name: :glukoza).first.elements
+      category_id = MeasurementCategory.where(name: :glukoza).first.id
+      imena = []
+      enote = []
+      maxs = []
+      mins = []
+      MeasurementType.where(measurement_category_id: category_id).all.each do |m|
+        imena.push(m.name)
+        enote.push(m.unit)
+        maxs.push(m.max_value)
+        mins.push(m.min_value)
+      end
+
       fields = "<div data-no-turbolink>"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
-      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Glukoza', u: 'mmol/L')
+
+      for i in 0..(st_elementov-1)
+        fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
+          ime = imena[i]
+          enota = enote[i]
+          min_value = mins[i]
+          max_value = maxs[i]
+          render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: ime, u: enota, min: min_value, max: max_value)
+        end
       end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
+
+      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za telesno temperaturo"
+      st_elementov = MeasurementCategory.where(name: :temperatura).first.elements
+      category_id = MeasurementCategory.where(name: :temperatura).first.id
+      imena = []
+      enote = []
+      maxs = []
+      mins = []
+      MeasurementType.where(measurement_category_id: category_id).all.each do |m|
+        imena.push(m.name)
+        enote.push(m.unit)
+        maxs.push(m.max_value)
+        mins.push(m.min_value)
+      end
+
       fields = "<div data-no-turbolink>"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
-      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Temperatura', u: 'stopinj celzija')
+
+      for i in 0..(st_elementov-1)
+        fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
+          ime = imena[i]
+          enota = enote[i]
+          min_value = mins[i]
+          max_value = maxs[i]
+          render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: ime, u: enota, min: min_value, max: max_value)
+        end
       end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
+
+      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     elsif name == "Dodaj meritev za telesno težo"
+      st_elementov = MeasurementCategory.where(name: :teza).first.elements
+      category_id = MeasurementCategory.where(name: :teza).first.id
+      imena = []
+      enote = []
+      maxs = []
+      mins = []
+      MeasurementType.where(measurement_category_id: category_id).all.each do |m|
+        imena.push(m.name)
+        enote.push(m.unit)
+        maxs.push(m.max_value)
+        mins.push(m.min_value)
+      end
+
       fields = "<div data-no-turbolink>"
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
-      fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
-        render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: 'Teza', u: 'kg')
+
+      for i in 0..(st_elementov-1)
+        fields += f.fields_for(association, new_object, child_index: id) do |measurement_docs_for_form|
+          ime = imena[i]
+          enota = enote[i]
+          min_value = mins[i]
+          max_value = maxs[i]
+          render(association.to_s.singularize + "_fields", f: measurement_docs_for_form, n: ime, u: enota, min: min_value, max: max_value)
+        end
       end
-      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button></div>"
+
+      fields += "<button type='button' class='btn btn-danger btn-xs' onclick='removeElement(this.parentNode);'>Odstrani</button><br><br></div>"
       link_to(name, '', class: "add_fields btn btn-info btn-xs", data: {no_turbolink: true,id: id, fields: fields.gsub("\n", "")})
     end
   end

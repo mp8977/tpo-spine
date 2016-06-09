fc03299ba47390fb8318b11f791168950105779e
mp8977 <mp8977@student.uni-lj.si>
2016-06-09 13:52:35 +0200

    Popravek taga sprememba gesla

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160606165119) do
-
 
   create_table "addresses", force: :cascade do |t|
     t.string   "streetName",   limit: 255
